f13bec3a54df28cb5a16f7da8e2e876815634c8d
bytetang <bytetang@users.noreply.github.com>
2026-07-29 14:40:51 +0000

    deploy: 6dc886fc71566b86893b3ba93ad735531dc6d6fc

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-e3763d09.js',
+                path_to_root + 'searchindex-3f065d6d.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
